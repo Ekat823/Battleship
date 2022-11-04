d2831e1619fd0bfe36f9313165b86d1f2ec75a91
Ekat <ekata823@gmail.com>
2022-11-04 15:57:49 +0300

    working on TableShips 3

---
+++ src/components/ButtonRearrange.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { BattleshipContext } from '../App';
+
+const ButtonRearrange = () => {
+
+  const battleshipContext = useContext(BattleshipContext)
+
+  return (
+    <button
+      className='btn-rearrange'
+      onClick={() => battleshipContext.dispatch({ type: 'TURN_ON_REARRANGE' })}
+    >
+      Rearrange
+    </button>
+  )
+}
+
+export default ButtonRearrange
--- src/components/TableShips.js
+++ src/components/TableShips.js
@@ -1,70 +1,68 @@
 import React, { useContext } from 'react';
 import { BattleshipContext } from '../App';
+import ButtonRearrange from './ButtonRearrange';
+import ButtonPlay from './ButtonPlay';
 
-const TableShips = ({handleClickChoose, handleClickPlay}) => {
+const TableShips = ({ handleClickChoose }) => {
 
   const battleshipContext = useContext(BattleshipContext)
 
-    const tableWidth = 1;
-    const tableHeight = battleshipContext.state.shipsNumber;
-       
-    const renderTable = (rows, columns) => {
-        
-        const buttons = {
-            shipBlack: 'btn-shipblack',
-            shipRed: 'btn-shipred',
-        }
+  const tableWidth = 1;
+  const tableHeight = battleshipContext.state.shipsNumber;
 
-        let result = [];
-        for(let i = 0; i < columns; i++) {
-            let c = [];
-            for(let j = 0; j < rows; j++) {
-                const index = i * rows + j;
-                c.push(
-                    <td key={index} onClick={handleClickChoose(index)}>
-                        {battleshipContext.state.playerShips[index] === 'placed' ? null : 
-                            <button 
-                                className={buttons[battleshipContext.state.playerShips[index]]} 
-                                style={{width:`${40 * battleshipContext.state.shipsSize[index] + 4}px`}} 
-                            />
-                        }
-                    </td>
-                );
+  const renderTable = (rows, columns) => {
+
+    const buttons = {
+      shipBlack: 'btn-shipblack',
+      shipRed: 'btn-shipred',
+    }
+
+    let result = [];
+    for (let i = 0; i < columns; i++) {
+      let c = [];
+      for (let j = 0; j < rows; j++) {
+        const index = i * rows + j;
+        c.push(
+          <td key={index} onClick={handleClickChoose(index)}>
+            {battleshipContext.state.playerShips[index] === 'placed' ? null :
+              <button
+                className={buttons[battleshipContext.state.playerShips[index]]}
+                style={{ width: `${40 * battleshipContext.state.shipsSize[index] + 4}px` }}
+              />
             }
-            result.push(<tr key={i}>{c}</tr>);
+          </td>
+        );
+      }
+      result.push(<tr key={i}>{c}</tr>);
+    }
+    //  console.log('blackFleet', blackFleet)
+    //  console.log('redFleet', redFleet)
+    return result;
+  };
+
+  return (
+    <div>
+      <p>
+        {battleshipContext.state.placeShipsMessage === 'click ship' ?
+          'Click a Ship'
+          : battleshipContext.state.placeShipsMessage === 'click cell' ?
+            'Click a Cell'
+            : battleshipContext.state.placeShipsMessage === 'play' ?
+              <>
+                <ButtonRearrange />
+                <br />
+                <ButtonPlay />
+              </>
+              : null
         }
-        //  console.log('blackFleet', blackFleet)
-        //  console.log('redFleet', redFleet)
-        return result;
-    };
-        
-    return (
-        <div>
-            <p>
-                {battleshipContext.state.placeShipsMessage === 'click ship' ? 
-                        'Click a Ship' 
-                    : battleshipContext.state.placeShipsMessage === 'click cell' ? 
-                        'Click a Cell'
-                    : battleshipContext.state.placeShipsMessage === 'play' ?
-                        <>
-                            <button className='btn-rearrange' onClick={() => battleshipContext.dispatch({ type: 'TURN_ON_REARRANGE' })}>
-                                Rearrange
-                            </button>
-                            <br />
-                            <button className='btn-play' onClick={() => battleshipContext.dispatch({ type: 'TURN_ON_PLAY' })}>
-                                Play
-                            </button>
-                        </>
-                    : null
-                }
-            </p>
-            <table className='style3'>
-                <tbody>
-                    {renderTable (tableWidth, tableHeight)}
-                </tbody>
-            </table>
-        </div>
-    )
+      </p>
+      <table className='style3'>
+        <tbody>
+          {renderTable(tableWidth, tableHeight)}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default TableShips
