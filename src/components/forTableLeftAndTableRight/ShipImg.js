997670adcf7c75d654105ccaf555e3fccd59000f
Ekat <ekata823@gmail.com>
2022-11-11 16:03:04 +0300

    new branch

--- src/components/forTableLeftAndTableRight/ShipImg.js
+++ src/components/forTableLeftAndTableRight/ShipImg.js
@@ -2,7 +2,7 @@ const ShipImg = ({el}) => {
     if(!el) {
         return null;
     }
-    return <img key={el.id} src={el.pic} alt={el.alt}/>;
+    return <img key={el.id} src={el.pic} alt={el.alt} style={{width: '2vw', height: '2vw'}}/>;
 }
 
 export default ShipImg
